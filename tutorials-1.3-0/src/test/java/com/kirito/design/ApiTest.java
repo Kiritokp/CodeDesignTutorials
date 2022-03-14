b999a4307087bb2380b79c8026cfd4415a92bfbe
kirito <1350221894@qq.com>
2022-03-14 09:13:50 +0800

    里氏替换原则设计模式

---
+++ tutorials-1.3-0/src/test/java/com/kirito/design/ApiTest.java
@@ -0,0 +1,39 @@
+package com.kirito.design;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+//import com.alibaba.fastjson.JSON;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class ApiTest {
+
+    private Logger logger = LoggerFactory.getLogger(ApiTest.class);
+
+    @Test
+    public void test_CashCard() {
+        CashCard cashCard = new CashCard();
+        // 提现
+        cashCard.withdrawal("100001", new BigDecimal(100));
+        // 储蓄
+        cashCard.recharge("100001", new BigDecimal(100));
+        // 交易流水
+        List<String> tradeFlow = cashCard.tradeFlow();
+        //logger.info("查询交易流水，{}", JSON.toJSONString(tradeFlow));
+    }
+
+    @Test
+    public void test_CreditCard() {
+        CreditCard creditCard = new CreditCard();
+        // 支付
+        creditCard.withdrawal("100001", new BigDecimal(100));
+        // 还款
+        creditCard.recharge("100001", new BigDecimal(100));
+        // 交易流水
+        List<String> tradeFlow = creditCard.tradeFlow();
+        //logger.info("查询交易流水，{}", JSON.toJSONString(tradeFlow));
+    }
+
+}
